89ca0d19965daff1222ca1f0b44207c97aaa23d2
Sakshi Kailas Nikam <sakshi.kailas.nikam@accenture.com>
2025-02-23 17:52:30 +0530

    homepage css changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,15 +18,16 @@ const HomePage = () => {
         <div className="intro">
           <div className="text-content animate-text">
 
-          <h1 data-testId = "headerText">Hey there, I'm <span className="highlight" style={{color:'yellow',fontSize:'3.3rem'}}>Sakshi</span>!</h1>
-          <p className="highlight">Your local Frontend Developer (and part-time code therapist for buggy JavaScript).</p>
-          <p className="highlight">With 4 years of experience, I’ve mastered:</p>
-          <p className="highlight">Writing code that works (and fixing it when it doesn’t).</p>
-          <p className="highlight">Arguing with CSS until it listens.</p>
-          <p className="highlight">Making websites so fun they’re borderline addictive.</p>
-          <p className="highlight" style={{color:'white'}}>My curly hair handles more tangles than your JSON files.</p>
-           
-           
+            <h1 data-testId="headerText">Hey there, I'm <span className="highlight" style={{ color: 'yellow', fontSize: '3.3rem' }}>Sakshi</span>!</h1>
+            <div className="highlight">
+              <p>Your local Frontend Developer (and part-time code therapist for buggy JavaScript).</p>
+              <p>With 4 years of experience, I’ve mastered:</p>
+              <p>Writing code that works (and fixing it when it doesn’t).</p>
+              <p>Arguing with CSS until it listens.</p>
+              <p>Making websites so fun they’re borderline addictive.</p>
+              <p style={{ color: 'white',fontSize:'1.3rem' }}>My curly hair handles more tangles than your JSON files.</p>
+            </div>
+
             <div className="call-to-action">
               <p>Let's build something amazing together!</p>
               <h1>Hire Me</h1>
@@ -34,7 +35,7 @@ const HomePage = () => {
           </div>
           <div className="image-container animate-text">
             <img
-              src={mee} 
+              src={mee}
               alt="Frontend Developer"
               className="profile-image"
             />
--- src/Components/Home.test.jsx
+++ src/Components/Home.test.jsx
@@ -4,7 +4,7 @@ import Home from './Home';
 describe("Home Component", () => {
   it("should display header", () => {
     render(<Home />);
-    const headerElement = screen.getByTestId("headerText"); // Now screen.getByTestId will work
-    expect(headerElement).toBeInTheDocument(); // Check if the element exists
+    const headerElement = screen.getByTestId("headerText"); 
+    expect(headerElement).toBeInTheDocument();
   });
 });
